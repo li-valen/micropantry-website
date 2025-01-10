ce3e2a001ef8adc415b12aa67a573afa6ea9c512
li-valen <li.valen.008@gmail.com>
2025-01-09 23:10:02 -0500

    auto scroll

--- assets/js/lenis.js
+++ assets/js/lenis.js
@@ -7,3 +7,66 @@ const lenis = new Lenis({
   lenis.on('scroll', (e) => {
     console.log(e);
   });
+
+const homeButton = document.getElementById("home");
+const homeElement = document.getElementById("home-page");
+
+homeButton.addEventListener("click", () => {
+  homeElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
+
+const contactButton = document.getElementById("contact");
+const contactElement = document.getElementById("contact-page");
+
+contactButton.addEventListener("click", () => {
+  contactElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
+
+const featureButton = document.getElementById("feature");
+const featureElement = document.getElementById("feature-page");
+
+featureButton.addEventListener("click", () => {
+  featureElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
+
+const storyButton = document.getElementById("story");
+const storyElement = document.getElementById("story-page");
+
+storyButton.addEventListener("click", () => {
+  storyElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
+
+const influencerButton = document.getElementById("influencer");
+const influencerElement = document.getElementById("influencer-page");
+
+influencerButton.addEventListener("click", () => {
+  influencerElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
+
+const faqButton = document.getElementById("faq");
+const faqElement = document.getElementById("faq-page");
+
+faqButton.addEventListener("click", () => {
+  faqElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
+
+const potentialButton = document.getElementById("potential");
+const potentialElement = document.getElementById("potential-page");
+
+potentialButton.addEventListener("click", () => {
+  potentialElement.scrollIntoView({
+    behavior: "smooth" // Optional for smooth scrolling
+  });
+});
