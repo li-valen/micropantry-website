c835b26d1871fd1493620ec8a54dc28502a953ed
li-valen <li.valen.008@gmail.com>
2025-02-16 08:34:26 -0500

    some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,9 @@ Author: GrayGrids
     //===== Prealoder
 
     window.onload = function () {
-        window.setTimeout(fadeout, 500);
+        window.setTimeout(fadeout, 2000);
     }
 
-    function fadeout() {
-        document.querySelector('.preloader').style.opacity = '0';
-        document.querySelector('.preloader').style.display = 'none';
-    }
 
 
     /*=====================================
