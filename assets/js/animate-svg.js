386daff2ce141d657192639716c6f008611ff25e
li-valen <li.valen.008@gmail.com>
2025-03-09 12:28:25 -0400

    added files

---
+++ assets/js/animate-svg.js
@@ -0,0 +1,48 @@
+function drawSVG() {
+	const pathElements = document.querySelectorAll("path");
+
+
+	const rectangles = document.querySelectorAll("rect");
+
+	rectangles.forEach((rectangle)=>{
+		rectangle.setAttribute("fill-opacity", "0");
+
+		setTimeout(()=>{
+			rectangle.style.stroke = "white";
+			rectangle.style.strokeWidth = "3";
+		},1000)
+	});
+
+	pathElements.forEach((path, index) => {
+		const pathLength = path.getTotalLength();
+		path.style.strokeDasharray = pathLength;
+		path.style.strokeDashoffset = pathLength;
+		path.style.fill = "none";
+		path.style.stroke = "none";
+		const drawingTime = pathLength * 0.003;
+
+
+		setTimeout(() => {
+			path.style.animation = `draw ${drawingTime}s ease-in forwards`;
+			path.style.stroke = "white";
+			path.style.strokeWidth ="3";
+	
+		}, drawingTime * index);
+
+		const style = document.createElement("style");
+		style.innerHTML = `
+        @keyframes draw {
+            from {
+                stroke-dashoffset: ${pathLength}/2;
+                }
+            to
+            {
+                stroke-dashoffset:0;
+            }
+        }`;
+        document.head.appendChild(style);
+
+	});
+}
+
+drawSVG();
